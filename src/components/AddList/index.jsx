7630db4f268f09b67640c808d7d2fc9bba1f4c50
Umutai <umutaiaitmamativa@gmail.com>
2022-05-12 15:02:15 +0300

    commit

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import List from "../List";
+import "../../components/AddList/AddListBotton.scss";
+import Badge from "../Badge";
+import closeSvg from "../../assets/img/close.svg";
+
+const AddButtonList = ({ colors, onAdd }) => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+  const [seletedColor, selectColor] = useState(3);
+  const [isLoading, setIsLoading] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (Array.isArray(colors)) {
+      selectColor(colors[0].id);
+    }
+  }, [colors]);
+
+  const onClose = () => {
+    setVisiblePopup(false);
+    setInputValue("");
+    selectColor(colors[0].id);
+  };
+  const addList = () => {
+    if (!inputValue) {
+      alert('Введите название списка');
+      return;
+    }
+
+    axios
+      .post("http://localhost:3001/lists", {
+        name: inputValue,
+        colorId: seletedColor,
+      })
+      .then(({ data }) => {
+        const color = colors.filter((c) => c.id === seletedColor)[0];
+        const listObj = { ...data, color, tasks: [] };
+
+        onAdd(listObj);
+        onClose();
+      })
+      .catch(() => {
+        alert('Ошибка при добавлении списка!');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  return (
+    <div className="add-list">
+      <List
+        onClick={() => setVisiblePopup(true)}
+        items={[
+          {
+            className: "list__add-button",
+            icon: (
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 1V15"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1 8H15"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            ),
+            name: "Добавить список",
+          },
+        ]}
+      />
+      {visiblePopup && (
+        <div className="add-list__popup">
+          <img
+            onClick={onClose}
+            src={closeSvg}
+            alt="close btn"
+            className="add-list__popup-close-btn"
+          />
+
+          <input
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            className="field"
+            type="text"
+            placeholder="Называние списка"
+          />
+
+          <div className="add-list__popup-colors">
+            {colors.map((color) => (
+              <Badge
+                onClick={() => selectColor(color.id)}
+                key={color.id}
+                color={color.name}
+                className={seletedColor === color.id && 'active'}
+              />
+            ))}
+          </div>
+          <button onClick={addList} className="button">
+            {isLoading ? 'Добавление...' : 'Добавить'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddButtonList;
---
+++ src/components/List/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./list.scss";
+import axios from "axios";
+import removeSvg from "../../assets/img/remove.svg";
+import Badge from "../Badge";
+import classNames from "classnames";
+
+const List = ({
+  items,
+  isRemovable,
+  onClick,
+  onRemove,
+  onClickItem,
+  activeItem,
+}) => {
+  const removeList = (item) => {
+    if (window.confirm("Вы действителбно хотите удалить список?")) {
+      axios.delete("http://localhost:3001/lists/" + item.id).then(() => {
+        onRemove(item.id);
+      });
+    }
+  };
+  return (
+    <ul onClick={onClick} className="list">
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={classNames(item.className, {
+            active:
+
+
+              activeItem && activeItem.id === item.id,
+          })}
+          onClick={onClickItem ? () => onClickItem(item) : null} 
+        >
+          <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
+
+
+          <span>
+            {item.name}
+            {item.tasks && `(${item.tasks.length})`}
+          </span>
+
+          {isRemovable && (
+            <img
+              className="list__remove-icon"
+              src={removeSvg}
+              alt="remove icon"
+              onClick={() => removeList(item)}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
